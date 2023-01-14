d8600985981569171999497229f057a822cd9575
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-01-14 21:42:25 +0330

    createfile complete

--- draft1.c
+++ draft1.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 
 char string[1000];
+char path2[1000];
 
 int does_exist(const char * path){
 
@@ -31,8 +32,6 @@ void createfile(){
         }
     }
 
-    //printf("%s\n", path);
-
     if (does_exist(path) == 1){
         printf("file already exists\n");
         return;
@@ -41,8 +40,21 @@ void createfile(){
     char * token = strtok(path, "/");
 
     while(token != NULL){
-        printf("%s\n", token);
+        strcat(path2, token);
+
         token = strtok(NULL, "/");
+        if (token != NULL){
+
+            if (does_exist(path2) == 0)
+                mkdir(path2);
+
+            strcat(path2, "/");
+        }
+
+        else{
+            FILE * filepointer = fopen(path2, "w");
+            fclose(filepointer);
+        }
     }
 }
 
@@ -77,12 +89,20 @@ void cat(){
 void removestr(){
 
     char path[300];
-    scanf("%s", path);
+
+    char c;
+    for (int i = 0; i<1000; i++){
+        c = getchar();
+        if (c != '\n')
+            path[i] = c;
+        else
+            break;
+    }
 
     char dashpos[10];
-    int line_num, start_pos;
-
     scanf("%s", dashpos);
+
+    int line_num, start_pos;
     scanf("%d:%d", &line_num, &start_pos);
 
     char dashsize[10];
@@ -106,7 +126,9 @@ void removestr(){
         }
     }
 
-    FILE * filepointer = fopen(path, "r+");
+    FILE * filepointer = fopen(path, "a");
+
+    rewind(filepointer);
 
     int line = 1;
     while(line != line_num){
@@ -116,9 +138,13 @@ void removestr(){
             line++;
     }
 
+    for(int i = 0; i<start_pos; i++){
+        char pos_count = fgetc(filepointer);
+    }
+
     if (strcmp(dashdir, "-f") == 0){
-        for (int i = 0; i<size; i++);
-            //complete this part
+        for (int i = 0; i<size; i++)
+            fprintf("%s", "\b \b");
     }
 
     fclose(filepointer);
@@ -128,7 +154,15 @@ void removestr(){
 void copystr(){
 
     char path[300];
-    scanf("%s", path);
+
+    char c;
+    for (int i = 0; i<1000; i++){
+        c = getchar();
+        if (c != '\n')
+            path[i] = c;
+        else
+            break;
+    }
 
     char dashpos[10];
     scanf("%s", dashpos);
@@ -179,7 +213,15 @@ void copystr(){
 void pastestr(){
 
     char path[300];
-    scanf("%s", path);
+    char c;
+
+    for (int i = 0; i<1000; i++){
+        c = getchar();
+        if (c != '\n')
+            path[i] = c;
+        else
+            break;
+    }
 
     char dashpos[10];
     scanf("%s", dashpos);
@@ -204,20 +246,92 @@ void pastestr(){
     rewind(filepointer);
 
     int line = 1;
-    char c;
+    char x;
 
     while(line != line_num){
 
-        c = fgetc(filepointer);
+        x = fgetc(filepointer);
+        if (x == '\n')
+            line++;
+    }
+
+    for(int i = 0; i<start_pos; i++){
+        char pos_count = fgetc(filepointer);
+    }
+
+    char sentence[1000];
+    strcpy(sentence, string);
+
+    fputs(sentence, filepointer);
+
+    fclose(filepointer);
+
+}
+
+void cutstr(){
+
+    char path[300];
+
+    char c;
+    for (int i = 0; i<1000; i++){
+        c = getchar();
+        if (c != '\n')
+            path[i] = c;
+        else
+            break;
+    }
+
+    char dashpos[10];
+    scanf("%s", dashpos);
+
+    int line_num, start_pos;
+    scanf("%d:%d", &line_num, &start_pos);
+
+    char dashsize[10];
+    scanf("%s", dashsize);
+
+    int size;
+    scanf("%d", &size);
+
+    char dashdir[5];
+    scanf("%s", dashdir);
+
+    if (path[0] == 34){
+        for (int i = 0; i<300; i++){
+            if (path[i+1] != 34)
+                path[i] = path[i+1];
+            else{
+                path[i] = NULL;
+                path[i+1] = NULL;
+                break;
+            }
+        }
+    }
+
+    FILE * filepointer = fopen(path, "a");
+
+    rewind(filepointer);
+
+    int line = 1;
+    while(line != line_num){
+
+        char c = fgetc(filepointer);
         if (c == '\n')
             line++;
     }
 
     for(int i = 0; i<start_pos; i++){
-        char c = fgetc(filepointer);
+        char pos_count = fgetc(filepointer);
     }
 
-    fputs(string, filepointer);
+    if (strcmp(dashdir, "-f") == 0){
+
+        for (int i = 0; i<size; i++)
+            string[i] = fgetc(filepointer);
+
+        for (int i = 0; i<size; i++)
+            fprintf("%s", "\b \b");
+    }
 
     fclose(filepointer);
 
@@ -235,19 +349,22 @@ int main(){
         scanf("%s%c", dashfile, &space);
 
         if (strcmp(operation, "createfile") == 0 && strcmp(dashfile, "--file") == 0 && space == 32)
-            createfile(); //not complete
+            createfile();
 
         else if (strcmp(operation, "cat") == 0 && strcmp(dashfile, "--file") == 0 && space == 32)
             cat();
 
         else if (strcmp(operation, "removestr") == 0 && strcmp(dashfile, "--file") == 0 && space == 32)
-            removestr(); //not done
+            removestr(); //complete backward
 
         else if (strcmp(operation, "copystr") == 0 && strcmp(dashfile, "--file") == 0 && space == 32)
             copystr(); //working only with -f not with -b but not with spaced path
 
         else if (strcmp(operation, "pastestr") == 0 && strcmp(dashfile, "--file") == 0 && space == 32)
             pastestr(); //it's not printing
+
+        else if (strcmp(operation, "cutstr") == 0 && strcmp(dashfile, "--file") == 0 && space == 32)
+            cutstr(); //complete backward
     }
 
 }
